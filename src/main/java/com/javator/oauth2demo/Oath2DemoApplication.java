a0da2b925900f4dcf1340753fcb6c7fb1eaf66e4
Inayath <inayathulla.khan.lavani@pwc.com>
2019-07-01 17:55:10 +0530

    First commit

---
+++ src/main/java/com/javator/oauth2demo/Oath2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.javator.oauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oath2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oath2DemoApplication.class, args);
+	}
+
+}
