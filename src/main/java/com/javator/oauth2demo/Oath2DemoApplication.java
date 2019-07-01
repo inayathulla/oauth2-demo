844fd8c9f0fda7acd6b370194f48db7bb26d0fc7
Inayath <inayathulla.khan.lavani@pwc.com>
2019-07-01 20:36:04 +0530

    Updated application.yml and added authorization server

--- src/main/java/com/javator/oauth2demo/Oath2DemoApplication.java
+++ src/main/java/com/javator/oauth2demo/Oath2DemoApplication.java
@@ -2,8 +2,10 @@ package com.javator.oauth2demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class Oath2DemoApplication {
 
 	public static void main(String[] args) {
