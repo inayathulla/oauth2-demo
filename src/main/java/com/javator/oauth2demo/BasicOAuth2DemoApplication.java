91ff2019f0da3334fccbb8432a209970a9124cf5
Inayath <inayathulla.khan.lavani@pwc.com>
2019-07-04 12:10:50 +0530

    This is basic authorization, where details are read from application.yml

--- src/main/java/com/javator/oauth2demo/BasicOAuth2DemoApplication.java
+++ src/main/java/com/javator/oauth2demo/BasicOAuth2DemoApplication.java
@@ -6,10 +6,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @SpringBootApplication
 @EnableAuthorizationServer
-public class OAuth2DemoApplication {
+public class BasicOAuth2DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OAuth2DemoApplication.class, args);
+		SpringApplication.run(BasicOAuth2DemoApplication.class, args);
 	}
 
 }
